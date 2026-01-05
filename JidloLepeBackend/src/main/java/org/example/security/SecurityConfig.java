75434ed8de6973f5f4ed806e9214fd0e0773ce10
Jitka Kroupova <50581008+Avarza@users.noreply.github.com>
2026-01-05 15:37:50 +0100

    funkční přihlášení, vypsání alergenů, vypsání openfoodfacts produktů, objevuje se overlay, neaktualizují se alergeny po zadání do fav.tsx

--- JidloLepeBackend/src/main/java/org/example/security/SecurityConfig.java
+++ JidloLepeBackend/src/main/java/org/example/security/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
+                                "/api/products/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/swagger-ui.html",
